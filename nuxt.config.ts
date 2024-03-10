c02602157adf7039c4a281ceb68840e1076c4bd1
Antek <tonylearnscoding@gmail.com>
2024-03-10 10:21:50 +0100

    [GR-1] Further app setup + create hero component

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,77 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
+
+import eslintPlugin from 'vite-plugin-eslint';
+
 export default defineNuxtConfig({
-  devtools: { enabled: true }
-})
+  app: {
+    head: {
+      title: 'GR Finanse',
+      meta: [
+        { charset: 'utf-8' },
+        { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+        {
+          hid: 'description',
+          name: 'description',
+          content: 'GR Finanse',
+        },
+      ],
+      link: [
+        { rel: 'preconnect', href: 'https://fonts.googleapis.com' },
+        {
+          rel: 'preconnect',
+          href: 'https://fonts.gstatic.com',
+          crossorigin: 'anonymous',
+        },
+        {
+          rel: 'stylesheet',
+          href: 'https://fonts.googleapis.com/css2?family=Poppins:wght@400;700;800&display=swap',
+        },
+        { rel: 'icon', type: 'image/png', href: '/favicon.ico' },
+      ],
+    },
+  },
+  css: ['@/assets/scss/common/index.scss'],
+  vite: {
+    plugins: [eslintPlugin({ failOnError: false })],
+    build: {
+      target: 'esnext',
+      commonjsOptions: {
+        transformMixedEsModules: true,
+      },
+    },
+    css: {
+      preprocessorOptions: {
+        scss: {
+          additionalData: '@import "assets/scss/global/main.scss";',
+        },
+      },
+    },
+  },
+  imports: {
+    dirs: ['composables/**'],
+  },
+  typescript: {
+    typeCheck: true,
+    strict: false,
+  },
+  runtimeConfig: {
+    public: {
+      baseUrl: process.env.BASE_URL,
+    },
+  },
+  modules: ['nuxt-vuefire'],
+  vuefire: {
+    config: {
+      apiKey: process.env.NUXT_PUBLIC_FIREBASE_API_KEY,
+      authDomain: process.env.NUXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+      projectId: process.env.NUXT_PUBLIC_FIREBASE_PROJECT_ID,
+      storageBucket: process.env.NUXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+      messagingSenderId: process.env.NUXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+      appId: process.env.NUXT_PUBLIC_FIREBASE_APP_ID,
+    },
+    auth: {
+      enabled: false,
+    },
+  },
+  devtools: { enabled: true },
+});
