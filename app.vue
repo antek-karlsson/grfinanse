a62b3ca4fc74fe413dffe02a5658567f4594ea7a
Antek <tonylearnscoding@gmail.com>
2024-04-16 21:51:04 +0200

    [GR-5] Add loader component

--- app.vue
+++ app.vue
@@ -1,6 +1,7 @@
 <template>
   <NuxtLayout>
-    <NuxtPage />
+    <NuxtPage v-if="!isLoading" />
+    <Loader v-else />
   </NuxtLayout>
 </template>
 
@@ -13,9 +14,24 @@ const { fetchTitleArray } = useTitleStore();
 const { fetchSections } = useSectionsStore();
 const { fetchFooter } = useFooterStore();
 
+const isLoading = ref(true);
+
 onMounted(async () => {
   await fetchTitleArray();
   await fetchSections();
   await fetchFooter();
+
+  const imageLoadPromises = Array.from(document.images).map((img) =>
+    img.complete && img.naturalHeight !== 0
+      ? Promise.resolve(true)
+      : new Promise((resolve) => {
+          img.onload = () => resolve(true);
+          img.onerror = () => resolve(false);
+        }),
+  );
+
+  Promise.all(imageLoadPromises).then((results) => {
+    isLoading.value = !results.every(Boolean);
+  });
 });
 </script>
